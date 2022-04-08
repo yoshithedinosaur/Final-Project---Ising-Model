23ab9b49c4bdb13a6578a4ad6a38eea7e24cc73e
yoshithedinosaur <yfujikake@hawk.iit.edu>
2022-04-08 13:09:01 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Yoshinobu Fujikake on 4/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
